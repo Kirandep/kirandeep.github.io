d2dcd76c875b8c5f14c62d5cb6e102643a79cc7e
Kirandep <114736527+Kirandep@users.noreply.github.com>
2022-09-30 19:54:04 +0530

    kirandeep

---
+++ js/script1.js
@@ -0,0 +1,23 @@
+var typed= new Typed(".typing",{
+    strings:["","","Web designer"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+ 
+ 
+ 
+ 
+ 
+ })
+
+ var typed= new Typed(".type",{
+    strings:["",""," Kirandeep Singh"],
+    typeSpeed:100,
+    BackSpeed:90,
+    loop:true
+ 
+ 
+ 
+ 
+ 
+ })
